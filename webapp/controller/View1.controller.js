4d90103ed643ed10f5831cbc77f33464deccf333
Jhony Gautam <lakshita.flow@gmail.com>
2025-12-18 12:54:24 +0000

    01. Created from template BASIC (Using Connect a system With Northwind odata)

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("northwinddemodest.controller.View1", {
+        onInit() {
+        }
+    });
+});
